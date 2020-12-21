11ea25988c9c6bb6b7f587855ec4d25260e28cb6
lfp <V18338258072@163.com>
2020-12-21 15:50:26 +0800

    完成登陆和注册功能开发

--- assets/js/login.js
+++ assets/js/login.js
@@ -57,13 +57,14 @@ $(function() {
     $.ajax({
       url: '/api/login',
       method: 'POST',
-      // 快速获取表单中的数据
+      // 快速获取表单中的数据使用serialize()函数
       data: $(this).serialize(),
       success: function(res) {
         if (res.status !== 0) {
           return layer.msg('登录失败！')
         }
         layer.msg('登录成功！')
+        console.log(res.token);
         // 将登录成功得到的 token 字符串，保存到 localStorage 中
         localStorage.setItem('token', res.token)
         // 跳转到后台主页
